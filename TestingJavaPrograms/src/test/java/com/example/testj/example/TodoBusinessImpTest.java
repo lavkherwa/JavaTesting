1b5e8ca5135703f7716dd015e5fcd14be3884280
apple <apple@localhost>
2020-04-05 00:29:29 +0530

    update

--- TestingJavaPrograms/src/test/java/com/example/testj/example/TodoBusinessImpTest.java
+++ TestingJavaPrograms/src/test/java/com/example/testj/example/TodoBusinessImpTest.java
@@ -1,12 +1,13 @@
 package com.example.testj.example;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-import static org.mockito.ArgumentMatchers.any;
 
 import java.util.Arrays;
 import java.util.List;
@@ -21,50 +22,58 @@ import com.example.testj.examples.TodoServiceImpl;
 @ContextConfiguration(classes = { TodoBusinessImpTest.class, TodoServiceImpl.class })
 public class TodoBusinessImpTest {
 
+	/* Unit testing using mocks and spy */
 	@Test
 	public void getTodosTest() {
 
 		// PROVIDED
-		/* Unit testing using mocks and spy */
-		TodoService todoService = spy(TodoServiceImpl.class);
-		// TodoService todoService = mock(TodoService.class);
+		TodoService todoService = mock(TodoServiceImpl.class);
 		TodoBusinessImpl todoBusinessImpl = spy(new TodoBusinessImpl(todoService));
 
 		// WHEN
 		when(todoService.getTodosForUser("lav")).thenReturn(Arrays.asList("lavTask1", "lavTask2"));
+		when(todoService.getTodos()).thenReturn(Arrays.asList("lavTask1", "lavTask2", "maniTask1", "maniTask2"));
 
 		// can only be used for VOID return type
 		// useful in cases where we need to call external service, example: auditing
 		doNothing().when(todoBusinessImpl).logForAudit(any());
 
+		// THEN
 		List<String> actualUserTodos = todoBusinessImpl.retrieveTodos("lav");
 		List<String> actualTodos = todoBusinessImpl.retrieveTodos();
 
-		// THEN
 		assertEquals(Arrays.asList("lavTask1", "lavTask2"), actualUserTodos, "user to do woked");
 		assertEquals(2, actualUserTodos.size(), "Ok");
 		verify(todoBusinessImpl, times(1)).retrieveTodos(any());
-		
+
 		assertEquals(Arrays.asList("lavTask1", "lavTask2", "maniTask1", "maniTask2"), actualTodos, "todos worked");
 		assertEquals(4, actualTodos.size(), "Ok");
 		verify(todoBusinessImpl, times(1)).retrieveTodos();
-		
-		
 	}
 
+	/* Integration testing using actual implementation */
 	@Test
 	public void getTodosTest2() {
 
 		// PROVIDED
-		/* Integration testing using actual implementation */
 		TodoService todoService = new TodoServiceImpl();
-		TodoBusinessImpl todoBusinessImpl = new TodoBusinessImpl(todoService);
+		TodoBusinessImpl todoBusinessImpl = spy(new TodoBusinessImpl(todoService));
 
 		// WHEN
-		List<String> actual = todoBusinessImpl.retrieveTodos("lav");
+		// Assume this will make an external API call and we want to avoid that ;)
+		doNothing().when(todoBusinessImpl).logForAudit(any());
 
 		// THEN
-		assertEquals(Arrays.asList("lavTask1", "lavTask2"), actual, "Cool");
+		List<String> actualUserTodos = todoBusinessImpl.retrieveTodos("lav");
+		List<String> actualTodos = todoBusinessImpl.retrieveTodos();
+
+		assertEquals(Arrays.asList("lavTask1", "lavTask2"), actualUserTodos, "user to do woked");
+		assertEquals(2, actualUserTodos.size(), "Ok");
+		verify(todoBusinessImpl, times(1)).retrieveTodos(any());
+
+		assertEquals(Arrays.asList("lavTask1", "lavTask2", "maniTask1", "maniTask2"), actualTodos, "todos worked");
+		assertEquals(4, actualTodos.size(), "Ok");
+		verify(todoBusinessImpl, times(1)).retrieveTodos();
 
 	}
 
